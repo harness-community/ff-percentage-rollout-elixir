d7855fb5da749bd529a36121f8250972c3409579
Erdi Rowlands <erdi.rowlands@harness.io>
2023-07-27 17:08:49 +0100

    FFM-8769 Update code

--- lib/elixir_sample/evaluation.ex
+++ lib/elixir_sample/evaluation.ex
@@ -4,13 +4,13 @@ defmodule ElixirPercentageRollout.Evaluation do
   require Logger
 
   def runPercentageRolloutEvaluations(flagIdentifier) do
-    evaluate_200k_unique_targets({0, 0, 0}, 0)
+    evaluate200kUniqueTargets(flagIdentifier, {0, 0, 0}, 0)
   end
 
-  def evaluate_200k_unique_targets(flagIdentifier, {variation1_counter, variation2_counter, variation3_counter}, 200_000) do
+  defp evaluate200kUniqueTargets(_, {variation1_counter, variation2_counter, variation3_counter}, 200_000) do
     {variation1_counter, variation2_counter, variation3_counter}
   end
-  def evaluate_200k_unique_targets(flagIdentifier, {variation1_counter, variation2_counter, variation3_counter}, accu_in) do
+  defp evaluate200kUniqueTargets(flagIdentifier, {variation1_counter, variation2_counter, variation3_counter}, accu_in) do
     counter = accu_in + 1
     target_identifier_number = Integer.to_string(counter)
     dynamic_target = %{
@@ -19,22 +19,22 @@ defmodule ElixirPercentageRollout.Evaluation do
       anonymous: ""
     }
     case :cfclient.string_variation(flagIdentifier, dynamic_target, "default") do
-      {:ok, _, "variation1"} ->
-        evaluate_200k_unique_targets(
+      {:ok, _, "rollout_variant_1"} ->
+        evaluate200kUniqueTargets(
           flagIdentifier,
           {variation1_counter + 1, variation2_counter, variation3_counter},
           counter
         )
 
-      {:ok, _, "variation2"} ->
-        evaluate_200k_unique_targets(
+      {:ok, _, "rollout_variant_2"} ->
+        evaluate200kUniqueTargets(
           flagIdentifier,
           {variation1_counter, variation2_counter + 1, variation3_counter},
           counter
         )
 
-      {:ok, _, "variation3"} ->
-        evaluate_200k_unique_targets(
+      {:ok, _, "rollout_variant_3"} ->
+        evaluate200kUniqueTargets(
           flagIdentifier,
           {variation1_counter, variation2_counter, variation3_counter + 1},
           counter
