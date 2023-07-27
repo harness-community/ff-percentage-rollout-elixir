f65a598021cfe9a1e4403aeebef19be904bca2a2
Erdi Rowlands <erdi.rowlands@harness.io>
2023-07-27 17:14:15 +0100

    FFM-8769 Update code

--- lib/elixir_sample/evaluation.ex
+++ lib/elixir_sample/evaluation.ex
@@ -4,15 +4,21 @@ defmodule ElixirPercentageRollout.Evaluation do
   require Logger
 
   def runPercentageRolloutEvaluations(flagIdentifier) do
-    final_counts = evaluate200kUniqueTargets(flagIdentifier, {0, 0, 0}, 0)
-    Logger.info("Final Counter Values: Variant 1: #{elem(final_counts, 0)}, Variant 2: #{elem(final_counts, 1)}, Variant 3: #{elem(final_counts, 2)}")
+    {count1, count2, count3} = evaluate200kUniqueTargets(flagIdentifier, {0, 0, 0}, 0)
+
+    percentage1 = Float.round(count1 / 200_000 * 100, 2)
+    percentage2 = Float.round(count2 / 200_000 * 100, 2)
+    percentage3 = Float.round(count3 / 200_000 * 100, 2)
+
+    Logger.info("Final Counter Values: Variant 1: #{count1}, Variant 2: #{count2}, Variant 3: #{count3}")
     Logger.info("""
     Final Percentage Values (rounded to 2 decimal places):
-    Variant 1: #{elem(final_counts, 0)} (#{Float.round(elem(final_counts, 0) / 200_000 * 100, 2)}%)
-    Variant 2: #{elem(final_counts, 1)} (#{Float.round(elem(final_counts, 1) / 200_000 * 100, 2)}%)
-    Variant 3: #{elem(final_counts, 2)} (#{Float.round(elem(final_counts, 2) / 200_000 * 100, 2)}%)
+    Variant 1: #{count1} (#{percentage1}%)
+    Variant 2: #{count2} (#{percentage2}%)
+    Variant 3: #{count3} (#{percentage3}%)
     """)
   end
+  
   defp evaluate200kUniqueTargets(_, {variation1_counter, variation2_counter, variation3_counter}, 200_000) do
     {variation1_counter, variation2_counter, variation3_counter}
   end
